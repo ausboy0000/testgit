6470d224190afae8846d6de8101dfdb3800392fe
ausboy0000 <64524511+ausboy0000@users.noreply.github.com>
2021-07-18 15:47:06 +1000

    test

--- src/yehd.java
+++ src/yehd.java
@@ -2,8 +2,11 @@ public class yehd {
 
 
     public static void main(String[] args) {
-        System.out.println("hshs");
+            System.out.println("hshs");
+//        System.out.printf();
     }
+
+
 }
 
 
